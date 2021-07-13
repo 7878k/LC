d5aee070920a59a3889663e971e5109e7d2fddb7
Weizheng Zhang <7878k@users.noreply.github.com>
2021-07-13 02:54:31 -0700

    update

--- Java/98_Validate Binary Search Tree.java
+++ Java/98_Validate Binary Search Tree.java
@@ -5,10 +5,10 @@ class Solution {
     private long prev = Long.MIN_VALUE; // 首先定义prev 无限的小
     
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;  // 如果root是null肯定是bst
+        if (root == null) return true;            // 二叉搜索树也可以为空！
         if (!isValidBST(root.left)) return false; // check 左子树是不是bst，如果不是就false
         if (root.val <= prev) return false;		  // 如果左子树是bst，check当前root值和prev的比较，prev是上一个节点值
-        prev = root.val;						  // 因为prev是全局变量，所以在测root.left之后，prev的值就变成root.left了，也就是root的上个值
+        prev = root.val;						  // 因为prev是全局变量，所以在测isValidBST(root.left))之后，prev的值就变成root.left了，也就是root的上个值
         return isValidBST(root.right);            // check 右子树是不是bst
     }
 }
