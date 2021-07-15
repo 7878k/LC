b6a9a086724b838d6ab47433cba16cf5499b8a54
Weizheng Zhang <7878k@users.noreply.github.com>
2021-07-14 18:51:06 -0700

    PERMUTATIONS

---
+++ Java/46_Permutations.java
@@ -0,0 +1,36 @@
+// 46. Permutations
+// Time & Space: O(N * N!)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null) {
+            return ret;
+        }
+        
+        dfs(nums, new boolean[nums.length], new ArrayList<>(), ret);
+        
+        return ret;
+        
+    }
+    
+    // 用visit记录当前index访问过没有
+    private void dfs(int[] nums, boolean[] visited, 
+                    List<Integer> path, List<List<Integer>> ret) {
+        if (nums.length == path.size()) {   // base case 当前path已经走完了
+            ret.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {      // 如果没有访问过，就加入当前path
+                path.add(nums[i]);  // choose
+                visited[i] = true;
+                
+                dfs(nums, visited, path, ret); // explore (recursion call)
+                // 回溯 跟之前做相反操作 unchoose         
+                visited[i] = false;
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
