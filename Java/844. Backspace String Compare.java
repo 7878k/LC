6a63a9179cb9e79a2c972eb68b6d679b3fec6552
Weizheng Zhang <7878k@users.noreply.github.com>
2021-07-27 14:22:52 -0700

    Update 844. Backspace String Compare.java
    O(1) solution

--- Java/844. Backspace String Compare.java
+++ Java/844. Backspace String Compare.java
@@ -21,3 +21,59 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+// Time: O(N + M); Space: O(1)
+// 双指针从后往前看，因为退格符只影响前面的那个字符
+// 设定两个skip记录2个string的退格符，如果碰到#， skip + 1
+// 如果碰到字符并且当前skip > 0， 那继续往前走 并且skip - 1
+
+class Solution2 {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            // 先找到 s 中第一个需要比较的字符（即去除 # 影响后的第一个待比较字符）
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            // 再找到 t 中第一个需要比较的字符（即去除 # 影响后的第一个待比较字符）
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // 只有 i j 都大于等于0才能比较
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+                // 如果有一个不是大于0的，那么说明有一个没找到待比较字符
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }
+            // 进行下一轮比较
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
+
