2c09f76850cb12d3517c572ba3ac07c32cb35c74
Frank Zhang <vvqqdd@gmail.com>
2021-10-23 16:48:44 -0700

    Create 0169. Majority Element.java

---
+++ Java/0169. Majority Element.java
@@ -0,0 +1,20 @@
+// 169. Majority Element
+// TIME: O(n); Space: O(1)
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
+
+
+
